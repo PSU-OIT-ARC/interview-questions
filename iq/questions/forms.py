916e65ddf98603fe75ec6e7cd4eb997f7a432dbf
Sean S <sisson@pdx.edu>
2015-04-20 22:31:19 +0000

    Tags need comma separation for select2, minor refactors

--- iq/questions/forms.py
+++ iq/questions/forms.py
@@ -12,33 +12,27 @@ class QuestionSearchForm(forms.Form):
     """
     Search for and filter through a list of questions
     """
-
     order = ChoiceField(required=False, label="")
     tags = CharField(required=False, label="")
 
     def __init__(self, *args, **kwargs):
+        requested_tags = kwargs.pop('requested_tags', None)
         super(QuestionSearchForm, self).__init__(*args, **kwargs)
-
+        self.fields['tags'].initial = requested_tags
         self.fields['tags'].widget.attrs['placeholder'] = "To filter by tags, enter tags here separated by spaces"
         self.fields['order'].choices = QuestionOrderChoices
-        self.fields['order'].initial = QuestionOrderChoices.CREATED_ON
 
     def search(self):
         questions = Question.objects.all()
-
         if self.is_bound and self.is_valid() and self.cleaned_data.get("order"):
-            if self.cleaned_data['order'] == str(QuestionOrderChoices.BODY):
+            if self.cleaned_data[ 'order' ] == str(QuestionOrderChoices.BODY):
                 questions = questions.order_by('body')
-            elif self.cleaned_data['order'] == str(QuestionOrderChoices.CREATED_ON):
+            if self.cleaned_data['order'] == str(QuestionOrderChoices.CREATED_ON):
                 questions = questions.order_by('-created_on')
-
-            #elif self.cleaned_data['order'] == str(QuestionOrderChoices.CREATED_BY):
-                #questions = questions.order_by('created_by')
-
-            elif self.cleaned_data['order'] == str(QuestionOrderChoices.DIFFICULTY):
+            if self.cleaned_data['order'] == str(QuestionOrderChoices.CREATED_BY):
+                questions = questions.order_by('created_by')
+            if self.cleaned_data['order'] == str(QuestionOrderChoices.DIFFICULTY):
                 questions = questions.order_by('difficulty')
-            else:
-                questions = questions.order_by('-created_on')
 
         return questions
 
@@ -47,7 +41,7 @@ class QuestionForm(forms.ModelForm):
     """
     Form (based on Question model) for editing/creating questions
     """
-    tags = forms.CharField(required=False, label="")
+    tags = forms.CharField(required=False, label="", widget=forms.widgets.HiddenInput)
     categories = forms.ModelMultipleChoiceField (
             queryset=Category.objects.all(),
             widget=forms.widgets.CheckboxSelectMultiple,
@@ -60,7 +54,6 @@ class QuestionForm(forms.ModelForm):
             'body',
             'answer',
             'difficulty',
-            #'created_by',
         )
 
     def __init__(self, *args, **kwargs):
@@ -82,34 +75,31 @@ class QuestionForm(forms.ModelForm):
         # If the question instance exists, associate categories with it
         if self.instance.pk is not None:
             cats = Category.objects.filter(categoryquestion__question=self.instance)
-            self.fields['categories'].initial = cats
+            self.fields["categories"].initial = cats
 
             # Order this question's tags in a comma separated list
             tags = self.instance.tags.all()
-            self.fields['tags'].initial = " ".join([str(tag) for tag in tags])
+            self.fields["tags"].initial = ", ".join([str(tag) for tag in tags])
 
     def clean_difficulty(self):
         """
-        Ensures difficulty is from 0-5
+        Ensures difficulty is a value from 0-5
         """
         diff = self.cleaned_data['difficulty']
-
         if diff is not None:
             if diff > 5 or diff < 0:
                 raise forms.ValidationError("Please enter a value from 1-5")
-
         return diff
 
     def clean_tags(self):
         """
-        Clean tags associated with the question
+        Clean associated tags
         """
         tags = self.cleaned_data['tags']
-        tags = set(tag.strip() for tag in re.split(r"\s+", tags))
+        tags = set(tag.strip() for tag in re.split(r"[,\s]+", tags))
         for tag in tags:
             if re.search("[^0-9A-Za-z]+", tag):
                 raise forms.ValidationError("This tag is invalid: %s" % tag)
-
         return tags
 
     def save(self, *args, **kwargs):
@@ -119,7 +109,6 @@ class QuestionForm(forms.ModelForm):
         """
         question = super(QuestionForm, self).save(*args, **kwargs)
         cats = []
-
         CategoryQuestion.objects.filter(question=self.instance).delete()
         for category in self.cleaned_data['categories']:
             cats.append(CategoryQuestion(category=category, question=self.instance))
