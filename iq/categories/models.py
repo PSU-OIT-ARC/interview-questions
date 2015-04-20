a16c27add5c55910db57f6a878d6650aabbc54b9
Sean S <sisson@pdx.edu>
2015-04-20 22:25:47 +0000

    Removed unused code in categories app

--- iq/categories/models.py
+++ iq/categories/models.py
@@ -4,22 +4,9 @@ class Category(models.Model):
     """
     Named category containing a list of associated questions
     """
-
-    # Colors are based off of bootstrap's message tag colors
-    BLACK = '#000000'      # Basic
-    WHITE = '#ffffff'      # Basic
-    BLUE = '#337ab7'       # Contrast: White
-    GREEN = '#5cb85c'      # Contrast: White
-    TURQUOISE = '#5bc0de'  # Contrast: White
-    YELLOW = '#f0ad4e'     # Contrast: White
-    RED = '#d9534f'        # Contrast: White
-    PURPLE = '#6f5499'     # Contrast: White
-
     category_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
-    color = models.CharField(max_length=16)
-    background_color = models.CharField(max_length=16)
     created_by = models.ForeignKey('auth.User', null=True, blank=True, on_delete=models.SET_NULL)
     created_on = models.DateTimeField(auto_now_add=True)
 
@@ -28,20 +15,3 @@ class Category(models.Model):
 
     class Meta:
         db_table = "category"
-
-    # I think we can scrap this
-    @staticmethod
-    def get_random_cat_colors():
-        """
-        Returns a random set of tag color combinations
-        """
-        COLOR_COMBINATIONS = (
-            (Tag.WHITE , Tag.BLUE),
-            (Tag.WHITE , Tag.GREEN),
-            (Tag.WHITE , Tag.TURQUOISE),
-            (Tag.WHITE , Tag.YELLOW),
-            (Tag.WHITE , Tag.RED),
-            (Tag.WHITE , Tag.PURPLE),
-        )
-
-        return random.choice(COLOR_COMBINATIONS)
